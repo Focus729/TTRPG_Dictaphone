b8958a4b0b0bd966415efdf891f5645ceb16bdef
Pavel <43194562+Focus729@users.noreply.github.com>
2026-08-13 12:38:40 +0000

    2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/TTRPG_Dictaphone/' : '/',
+
   plugins: [react()],
-  server: { proxy: { '/api': 'http://127.0.0.1:3000' } },
-})
+
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:3000',
+    },
+  },
+}))
